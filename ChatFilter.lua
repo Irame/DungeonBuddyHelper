eb96083287021caa1253001613458786a8635b5b
Felix <irame94@gmail.com>
2025-03-14 20:31:24 +0100

    added [LFG NOP] after keystone links in party chat

---
+++ ChatFilter.lua
@@ -0,0 +1,23 @@
+---@class DBH_Private
+local private = select(2, ...)
+
+local function PartyChatFilter(self, event, msg, sender, ...)
+    local mythicKeyData = msg:match("|Hkeystone:([^|]+)|h")
+    if mythicKeyData then
+        local clickableLink = "|HDungeonBuddyHelper:" .. mythicKeyData .. "|h[LFG NOP]|h"
+        msg = msg .. " " .. clickableLink
+    end
+    return false, msg, sender, ...
+end
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_PARTY", PartyChatFilter)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_PARTY_LEADER", PartyChatFilter)
+
+local function CustomLinkClicked(_, link)
+    local mythicKeyData = link:match("DungeonBuddyHelper:(.+)")
+    if mythicKeyData then
+        private.addon:ShowLFGFrameAndDiscordCommand("keystone:" .. mythicKeyData)
+    end
+end
+
+hooksecurefunc(ItemRefTooltip, "SetHyperlink", CustomLinkClicked)
